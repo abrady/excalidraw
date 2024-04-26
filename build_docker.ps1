e96222d91f7ca50c91d0f1ab7c9d31a930514a14
Aaron Brady <abrady@meta.com>
2024-04-25 21:55:21 -0700

    docker creation

--- build_docker.ps1
+++ build_docker.ps1
@@ -3,5 +3,8 @@ $env:VITE_APP_DISABLE_TRACKING = $true
 $env:VITE_APP_ENABLE_ESLINT = "false"
 echo $env:VITE_APP_ENABLE_ESLINT
 cd excalidraw-app
-npx vite build --out-dir ../build --mode production
+#npx vite build --out-dir ../build --mode production
+npx vite build --out-dir ../build --mode development
+
 cd ..
+docker build -t excalidraw-app .
